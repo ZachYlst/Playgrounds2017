4b90c2502ea52b37f80519ffb819f4e936d5541e
ZachYlst <ZachYlst@users.noreply.github.com>
2017-09-25 08:36:46 -0600

    Tested variables and values.

--- IntroSwift.playground/Contents.swift
+++ IntroSwift.playground/Contents.swift
@@ -1,5 +1,27 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
+var ramen : String
+    //Explicit type declaration with no value
+var soup = "ramen"
+    //Implicit type declaration with assigned value type
+ramen = soup
+print(ramen)
+print(soup)
+var myAge : Double
+myAge = 17
+print(myAge)
+myAge = myAge * 3.14
+var someValue = 3.14159 * 56
+someValue += myAge
+someValue = someValue * 560.6
+var isReal : Bool
+isReal = false
+isReal = true
+let myName : String
+myName = "Zachary"
+print(myName)
+//myName = "soup"
+    //Can't assign into a let once assigned
 
-var str = "Hello, playground"
+var object : Double = 3.14159
+object * 2
+object + 3.14159
