c76dfef30cc63b8bd996681db8c01bb30d153888
ZachYlst <ZachYlst@users.noreply.github.com>
2017-09-27 09:11:16 -0600

    Added more methods to the project.

--- MethodsPractice.playground/Contents.swift
+++ MethodsPractice.playground/Contents.swift
@@ -21,3 +21,34 @@ evenLessSimpleMethod(count : 4,
                      bandName : "They Might Be Giants",
                      date : "June, 1992")
 
+public func calculateDays(first firstDay : Int,
+                          secondDay : Int) -> Int
+        //first and firstDay = same thing
+{
+    let answer = secondDay - firstDay
+    return answer
+}
+calculateDays(first : 4,
+              secondDay : 5)
+public func name(friendName homeName : String) -> Void
+{
+    let info = "At home, my name is " + homeName
+    print(info)
+}
+name(friendName : "Zach")
+
+var guess = 453
+if (guess < 500)
+{
+    print("Wahoo!")
+}
+else if(guess == 65)
+{
+    print("(not) Wahoo!")
+}
+while (guess > 0)
+{
+    print("teehee \(guess)")
+    guess -= 1
+        //guess is >0 by 453, so it prints 453 times
+}
